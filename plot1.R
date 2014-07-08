f93eeffb560a040d8de4fb31cc655761e3a177cc
cyberis <christopher.bortz@gmail.com>
2014-07-07 20:55:13 -0400

    Initial skeleton files created

---
+++ plot1.R
@@ -0,0 +1,5 @@
+# plot1.R - Plot the First Graphic
+# Submitted by Christopher Bortz
+# For Exploratory Data Analysis Section 4 - Dr. Roger Peng
+# July 7 - August 4, 2014
+# Course Project 1 - Electrical Power Consumption
---
+++ plot4.R
@@ -0,0 +1,5 @@
+# plot4.R - Plot the Fourth Graphic
+# Submitted by Christopher Bortz
+# For Exploratory Data Analysis Section 4 - Dr. Roger Peng
+# July 7 - August 4, 2014
+# Course Project 1 - Electrical Power Consumption
